db206eb32328a436e03b5acf61d4c7db0cb2d90c
Raul Alejandro Salas Texido <alejandrosalas0891@gmail.com>
2020-04-01 09:39:05 +0200

    [ADD] Adding Comments table and relationships

---
+++ database/migrations/2020_04_01_073450_create_comments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('ticket_id')->unsigned()->required();
+            $table->unsignedBigInteger('user_id')->required();
+            $table->longText('comment')->required();
+
+            $table->timestamps();
+            $table->unsignedBigInteger('created_by')->index();
+            $table->unsignedBigInteger('updated_by')->index();
+        
+            //relationships
+            $table->foreign('ticket_id')->reference('id')->on('tickets');
+            $table->foreign('user_id')->reference('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
