f4225f87744afa98893fe3bccb18daac6a7ef2f6
Raul Alejandro Salas Texido <alejandrosalas0891@gmail.com>
2020-04-01 09:17:54 +0200

    First Commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('firstname');
+            $table->string('lastname')->nullable();
+            $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('start_date')->required();
+            $table->timestamp('end_date')->required();
+            $table->rememberToken();
+            $table->timestamps(); //esto crea automaticamente los campos create_by and updated_by
+            $table->unsignedBigInteger('created_by')->index();
+            $table->unsignedBigInteger('updated_by')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2020_04_01_071115_create_tickets_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('type_id')->required();
+            $table->unsignedSmallInteger('priority_id')->required();
+            $table->unsignedBigInteger('requester_user_id')->required();
+            $table->string('name')->required();
+
+            // comment _id
+            $table->boolean('flag_status')->default(true); //true=open or false=close
+            
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
