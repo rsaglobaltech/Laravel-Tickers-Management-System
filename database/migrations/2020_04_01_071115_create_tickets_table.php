c5e029ae7a3ba9114117ba4b9082fa4b435d2fd8
Raul Alejandro Salas Texido <alejandrosalas0891@gmail.com>
2020-04-01 09:33:45 +0200

    [ADD] Creating relationship between tickets, users, types and priority tables

--- database/migrations/2020_04_01_071115_create_tickets_table.php
+++ database/migrations/2020_04_01_071115_create_tickets_table.php
@@ -15,15 +15,21 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->unsignedSmallInteger('type_id')->required();
+            $table->unsignedSmallInteger('type_id')->nullable();
             $table->unsignedSmallInteger('priority_id')->required();
             $table->unsignedBigInteger('requester_user_id')->required();
             $table->string('name')->required();
-            
+
             $table->boolean('flag_status')->default(true); //true=open or false=close
+            
+            $table->timestamps();
+
             $table->unsignedBigInteger('created_by')->index();
             $table->unsignedBigInteger('updated_by')->index();
-            $table->timestamps();
+            
+            $table->foreign('type_id')->reference('id')->on('types')->onDelete('set null');
+            $table->foreign('priority_id')->reference('id')->on('priorities');
+            $table->foreign('requester_user_id')->reference('id')->on('users');
         });
     }
 
